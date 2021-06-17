7935cfd27d041755a71fb0b34d8a750df81aa484
adrian stiven <jimenezcardenasad@gmail.com>
2021-06-16 21:14:03 -0500

    aprendiendo diccionarios en python

--- pruebas.py
+++ pruebas.py
@@ -31,7 +31,7 @@
 # print(miLista.index("adrian"))
 
 
-# # ejerccios de tupla
+# ###### ejerccios de tupla
 # tupla = ("juan", 13, 1, 1995)
 # print(tupla[2])
 
@@ -45,6 +45,30 @@
 # print(milista.count("juan"))
 # # contar cuantos caracteres tien en este caso mi listas
 # print(len(milista))
+
+# ####### ejerccios de diccionario
+# midiccionario = {"alemania": "berlin", "francia": "paris", 23: "jordan",
+#                  "reino unido": "londres", "esapña": "madrid"}
+# print(midiccionario["esapña"])
+# # agregando mas paise y su capital
+# midiccionario["colombia"] = "barranquillla"
+# print(midiccionario)
+# # cambiandole la capital a colombia
+# midiccionario["colombia"] = "bogota"
+# print(midiccionario)
+# # eliminando elemto ,reino unido y su capital
+# del midiccionario["reino unido"]
+# print(midiccionario)
+# middicionario = {23: "jordan", "nombre": "michael", "adrian": "stiven",
+#                  "brayan": "andres", "anillos": {"temporadas": [1991, 1992, 1993, 1994, 1995, 1996]}}
+# # ingresar a 1993 jajja imbentado por mi
+# print(middicionario["anillos"]["temporadas"][2])
+# # imprimir todas las llaver del diccionario
+# print(middicionario.keys())
+# # imprimir los valores de las llaves
+# print(middicionario.values())
+# print(len(middicionario))
+
 
 # ##### ciclo while
 # i=1
