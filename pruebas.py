8e43b4d29bf63e5d119f3467044b477b697a3a09
adrian stiven <jimenezcardenasad@gmail.com>
2021-06-17 22:10:04 -0500

    profundizando en el tema de excepciones

--- pruebas.py
+++ pruebas.py
@@ -308,10 +308,7 @@
 # # yield contien los numeros
 #         yield numero1*2
 #         numero1 += 1
-
-
 # contenedordefuncion=generapares(10)
-
 # print(next(contenedordefuncion))
 # print("aqui podria ir mas codigo....")
 # print(next(contenedordefuncion))
@@ -330,7 +327,31 @@
 # print(next(contendo_ciudadaes))
 # print(next(contendo_ciudadaes))
 
-#######ejerccios de extenciones
+# ###### ejerccios de excepciones
+# numero1= int(input("dijite un numero 1: "))
+# numero2= int(input("dijite un numero 2: "))
+# def divide(numero1, numero2):
+#     #sirve por si ahy un error, siga corriendo el codigo normal
+#     try:
+#         return numero1 / numero2
+#     except  ZeroDivisionError:
+#         print("lo sinto no se puede dividir por cero")
+# print(divide(numero1, numero2))
+# print("gracias por insertar los numeros")
 
+# ######
+# def divide():
+#     try:
+#         op1=float(input("introduce el primer numero: "))
+#         op2=float(input("introduce el segundo numero: "))
+#         print("la divison es " + str(op1/op2))
+#     except ValueError:
+#         print("el valor introducido es erroneo")
+#         #pone esto mas el nombre del ERROR que te da en la consola
+#     except ZeroDivisionError:
+#         print("no se puede dividir por cero")
+#         #hacer qu esta linea se ejecute siempre con finally
+#     finally:
+#         print("calculo finalizado")
 
-
+# divide()
